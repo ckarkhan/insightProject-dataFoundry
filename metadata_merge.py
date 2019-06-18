f46f29fff0706b0f71411352fe57627d9c18d6ec
Chetan Karkhanis <chetanhome@Chetans-Personal-Mac.local>
2019-06-18 14:40:14 -0700

    pyspark example file commit

--- metadata_merge.py
+++ metadata_merge.py
@@ -2,3 +2,15 @@
 Code to merge patient data with their ailment data.
 """
 
+from pyspark.sql import SparkSession
+
+DE_File = "/usr/local/spark/README.md"
+spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
+logData = spark.read.text(logFile).cache()
+
+numA = logData.filter(logData.value.contains('a')).count()
+numB = logData.filter(logData.value.contains('b')).count()
+
+print("Lines with a: %i, lines with b: %i"  % (numA, numB))
+
+spark.stop()
