2c8c28e0c76cf8842ae82d7694c3ee901e2610e3
Chetan Karkhanis <chetanhome@Chetans-Personal-Mac.local>
2019-06-26 10:11:24 -0700

    Trying to build a homepage

--- xray/app/routes.py
+++ xray/app/routes.py
@@ -1,6 +1,16 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Insight"
+    user = {'username': 'Chetan'}
+    return '''
+<html>
+    <head>
+        <title>"</title>
+    </head>
+    <body>
+        <h1></h1>Hello, ''' + user['username'] + ''' Welcome to X-ray Annotations. You are contributing to better clinical diagnosis!</h>
+    </body>
+</html>'''
