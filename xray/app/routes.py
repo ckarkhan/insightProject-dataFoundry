a8ef0e9f5f7957b68f300b980d32fedfb20da664
Chetan Karkhanis <chetanhome@Chetans-Personal-Mac.local>
2019-06-26 12:17:51 -0700

    Homepage build

--- xray/app/routes.py
+++ xray/app/routes.py
@@ -5,12 +5,4 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Chetan'}
-    return '''
-<html>
-    <head>
-        <title>"</title>
-    </head>
-    <body>
-        <h1></h1>Hello, ''' + user['username'] + ''' Welcome to X-ray Annotations. You are contributing to better clinical diagnosis!</h>
-    </body>
-</html>'''
+    return render_template('index.html', title='Home', user=user)
