8aaa0c389b5a73846c6102675894d9ff66bdb2c7
Chetan Karkhanis <chetanhome@Chetans-Personal-Mac.local>
2019-06-28 15:49:18 -0700

    User friendly message for incorrect file format

--- xray/app/routes.py
+++ xray/app/routes.py
@@ -16,16 +16,19 @@ def load_image():
 def getOneImageData(imgId):
     """Function to retrieve one image's data from elasticseach."""
 
-    host = 'http://10.0.0.13:9200'
-    index = 'xray_chest'
-    doc_type = 'staff_notes'
-    es = Elasticsearch(host)
-    file_id = '000' + str(imgId) + '.png'
+    if len(imgId) != 9:
+        resp1 = {imgId: "Format error! Please use format '00000_000' "}
+    else:
+        host = 'http://10.0.0.13:9200'
+        index = 'xray_chest'
+        doc_type = 'staff_notes'
+        es = Elasticsearch(host)
+        file_id = '000' + str(imgId) + '.png'
 
-    s3_loc = 'https://chest-xray-source-images.s3-us-west-2.amazonaws.com/image_store/' + file_id
-   
-    resp = es.get(index=index, doc_type=doc_type, id=file_id)
-    resp1 = resp['_source']
-    resp1.update({'Image_View': s3_loc})
+        s3_loc = 'https://chest-xray-source-images.s3-us-west-2.amazonaws.com/image_store/' + file_id
+    
+        resp = es.get(index=index, doc_type=doc_type, id=file_id)
+        resp1 = resp['_source']
+        resp1.update({'Image_View': s3_loc})
 
     return render_template('one_image.html',  result = resp1)
