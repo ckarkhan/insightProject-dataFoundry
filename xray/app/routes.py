146873582c55b142c1143ac4d553e4b4faf73175
Chetan Karkhanis <chetanhome@Chetans-Personal-Mac.local>
2019-07-01 13:27:46 -0700

    Catching an exception gracefully with a try block

--- xray/app/routes.py
+++ xray/app/routes.py
@@ -26,9 +26,12 @@ def getOneImageData(imgId):
         file_id = '000' + str(imgId) + '.png'
 
         s3_loc = 'https://chest-xray-source-images.s3-us-west-2.amazonaws.com/image_store/' + file_id
-    
-        resp = es.get(index=index, doc_type=doc_type, id=file_id)
-        resp1 = resp['_source']
-        resp1.update({'Image_View': s3_loc})
+
+        try:
+            resp = es.get(index=index, doc_type=doc_type, id=file_id)
+            resp1 = resp['_source']
+            resp1.update({'Image_View': s3_loc})
+        except Exception as ex:
+            resp1 = {imgId: ex}
 
     return render_template('one_image.html',  result = resp1)
