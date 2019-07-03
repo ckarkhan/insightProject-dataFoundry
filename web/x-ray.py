8194a4e788fbfdca6988c78ced5114a60878c6f8
Chetan Karkhanis <chetanhome@Chetans-Personal-Mac.local>
2019-07-02 18:47:11 -0700

    Importing dependencies

--- web/x-ray.py
+++ web/x-ray.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from elasticsearch import Elasticsearch
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
