99d25ccc5cda8c9247470efc1ba4bf5dfd875985
Chetan Karkhanis <chetanhome@ChetanssonalMac.attlocal.net>
2019-07-03 00:41:36 -0700

    Basic Page Layout

--- web/app.py
+++ web/app.py
@@ -1,32 +1,57 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+
 from elasticsearch import Elasticsearch
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Welcome to X-Ray Annotations!',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
         }
-    )
+    ),
+
+    html.Div(children='Thank You for being a part of the community.', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+
+    dcc.Tabs(id="tabs-styled-with-props" , value='tab-1', children=[
+        dcc.Tab(label='Single Image Search', value='tab-1'),
+        dcc.Tab(label='Multi Image Search', value='tab-2'),
+    ], colors={
+        "border": "white",
+        "primary": "gold",
+        "background": "grey"
+    }),
+    html.Div(id='tabs-content-props')
 ])
+
+@app.callback(Output('tabs-content-props', 'children'),
+              [Input('tabs-styled-with-props', 'value')])
+def render_content(tab):
+    if tab == 'tab-1':
+        return html.Div([
+            html.H3('Tab content 1')
+        ])
+    elif tab == 'tab-2':
+        return html.Div([
+            html.H3('Tab content 2')
+        ])
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True, host = "0.0.0.0")
